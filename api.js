f991c879d30219d987d35e0ad7513c61246ac445
Carlossperea <142156077+Carlossperea@users.noreply.github.com>
2023-08-30 00:58:44 -0300

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,37 @@
+/*API DÓLAR SI*/
 
+/*PRECIO DÓLAR BLUE (VENTA)*/
+
+fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
+    .then(datos=>datos.json())
+    .then(
+        datos => {
+            const HTMLResponse = document.querySelector("#cotDolarV");
+            HTMLResponse.dataset.cotizacion = datos[1]['casa']['venta'];
+            HTMLResponse.innerHTML = '$ ' + datos[1]['casa']['venta'];
+        }
+    )
+
+    /*PRECIO DÓLAR BLUE (COMPRA)*/
+
+    fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
+    .then(datos=>datos.json())
+    .then(
+        datos => {
+            const HTMLResponse = document.querySelector("#cotDolarC");
+            HTMLResponse.dataset.cotizacion = datos[1]['casa']['compra'];
+            HTMLResponse.innerHTML = '$ ' + datos[1]['casa']['compra'];
+        }
+    )
+
+    /*PRECIO DÓLAR OFICIAL (VENTA)*/
+
+    fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
+    .then(datos=>datos.json())
+    .then(
+        datos => {
+            const HTMLResponse = document.querySelector("#cotDolar");
+            HTMLResponse.dataset.cotizacion = datos[0]['casa']['venta'];
+            HTMLResponse.innerHTML = '$ ' + datos[0]['casa']['venta'];
+        }
+    )
